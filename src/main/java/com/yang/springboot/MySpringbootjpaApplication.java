11fbd9c23ed8c79344e78070f7a05123ede5b49b
Jing Yang <yangjing115@wanda.cn>
2017-09-05 15:22:32 +0800

    添加spring amqp来做消息管理：发送消息到控制台

--- src/main/java/com/yang/springboot/MySpringbootjpaApplication.java
+++ src/main/java/com/yang/springboot/MySpringbootjpaApplication.java
@@ -1,14 +1,32 @@
 package com.yang.springboot;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching//如果要使用缓存机制，在spring boot中还是要使用@EnableCaching
-public class MySpringbootjpaApplication {
+public class MySpringbootjpaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringbootjpaApplication.class, args);
-	}
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public Queue wiselyQueue() {
+        return new Queue("my-queue");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        rabbitTemplate.convertAndSend("my-queue", "来自RabbitMQ的问候！");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootjpaApplication.class, args);
+    }
 }
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table people
-(
-id number not null primary key,
-name varchar2(20),
-age varchar2(3),
-nation varchar2(20),
-address varchar2(20)
-);
